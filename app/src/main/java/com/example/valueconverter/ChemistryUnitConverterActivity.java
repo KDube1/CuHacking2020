3222d53e8bc85c2596caa9c09c5532b4c6837e4f
KDube1 <krishna.dube1@gmail.com>
2020-01-11 17:26:11 -0500

    added spinner code

--- app/src/main/java/com/example/valueconverter/ChemistryUnitConverterActivity.java
+++ app/src/main/java/com/example/valueconverter/ChemistryUnitConverterActivity.java
@@ -3,7 +3,9 @@ package com.example.valueconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Spinner;
 
 import java.math.*;
 
@@ -13,9 +15,22 @@ public class ChemistryUnitConverterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chemistry_unit_converter);
 
-        addListenerOnSpinnerItemSelection();
-        addListenerOnButton();
+        Spinner spinner = (Spinner) findViewById(R.id.firstUnitSpinner);
+
+
     }
 
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+        // An item was selected. You can retrieve the selected item using
+        // parent.getItemAtPosition(pos)
 
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
+    }
 }
+
+
+
