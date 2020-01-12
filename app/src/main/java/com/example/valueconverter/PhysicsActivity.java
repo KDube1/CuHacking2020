fa803f36716dcb35dad4a637fd142847db519169
Spoonter <33159009+Spoonter@users.noreply.github.com>
2020-01-12 02:42:05 -0500

    memes

--- app/src/main/java/com/example/valueconverter/PhysicsActivity.java
+++ app/src/main/java/com/example/valueconverter/PhysicsActivity.java
@@ -16,36 +16,39 @@ public class PhysicsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_physics);
     }
 
-    private void getVelocity(View view){
+    private double getVelocity(View view){
         TextView eText = (TextView) findViewById(R.id.VelocityEdit);
         velocity = Double.parseDouble(eText.getText().toString());
+        return velocity;
     }
 
-    private void getAngle(View view){
+    private double getAngle(View view){
         TextView eText = (TextView) findViewById(R.id.AngleEdit);
         angle = Double.parseDouble(eText.getText().toString());
+        return angle;
     }
 
-    private void getHeight1(View view){
+    private double getHeight1(View view){
         TextView eText = (TextView) findViewById(R.id.HeightEdit);
         height1 = Double.parseDouble(eText.getText().toString());
+        return height1;
     }
 
     private double calculateTime(View view){
-        double t = -velocity*Math.sin(angle*(Math.PI/180)) + Math.sqrt(Math.pow(velocity*Math.sin(angle*(Math.PI/180)),2) - 2*(-9.81)*(height1));
+        double t = -getVelocity(view)*Math.sin(getAngle(view)*(Math.PI/180)) + Math.sqrt(Math.pow(getVelocity(view)*Math.sin(getAngle(view)*(Math.PI/180)),2) - 2*(-9.81)*(getHeight1(view)));
         TextView eText = (TextView) findViewById(R.id.Time);
         eText.setText(Double.toString(t)+ " is the flight time");
         return t;
     }
 
     private void calculateDistance(View view){
-        double d = velocity*Math.cos(angle*(Math.PI/180)*calculateTime(view));
+        double d = getVelocity(view)*Math.cos(getAngle(view)*(Math.PI/180)*calculateTime(view));
         TextView eText = (TextView) findViewById(R.id.Distance);
         eText.setText(Double.toString(d)+ " is the distance travelled");
     }
 
     private void calculateMaxHeight(View view){
-        double hMax = (Math.pow(velocity,2)*Math.pow(Math.sin(angle*(Math.PI/180)),2))/(2*9.81);
+        double hMax = getHeight1(view) +(Math.pow(getVelocity(view),2)*Math.pow(Math.sin(getAngle(view)*(Math.PI/180)),2))/(2*9.81);
         TextView eText = (TextView) findViewById(R.id.MaxHeight);
         eText.setText(Double.toString(hMax) + " is the max height");
     }
