e707ce979633d01a140a9ce1b92a4b2921630780
KDube1 <krishna.dube1@gmail.com>
2020-01-11 23:23:20 -0500

    minor changes

--- app/src/main/java/com/example/valueconverter/ChemistryUnitConverterActivity.java
+++ app/src/main/java/com/example/valueconverter/ChemistryUnitConverterActivity.java
@@ -6,51 +6,40 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 import java.math.*;
 
 public class ChemistryUnitConverterActivity extends AppCompatActivity {
+    String convertTypeFrom;
+    String convertTypeTo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chemistry_unit_converter);
 
-        Spinner spin = (Spinner) findViewById(R.id.firstUnitSpinner);
-        spin.setOnItemSelectedListener(new OnItemSelectedListener() {
-
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view,
-                                       int position, long id) {
-                Object item = adapterView.getItemAtPosition(position);
-                if (item != null) {
-                    Toast.makeText(ChemistryUnitConverterActivity.this, item.toString(),
-                            Toast.LENGTH_SHORT).show();
-                }
-                Toast.makeText(ChemistryUnitConverterActivity.this, "Selected",
-                        Toast.LENGTH_SHORT).show();
-
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-                // TODO Auto-generated method stub
-
-            }
-        });
-    }
-
-    public void onItemSelected(AdapterView<?> parent, View view,
-                               int pos, long id) {
-        // An item was selected. You can retrieve the selected item using
-        // parent.getItemAtPosition(pos)
 
     }
 
-    public void onNothingSelected(AdapterView<?> parent) {
-        // Another interface callback
+
+    public void goConvert(View view){
+        RadioGroup typeRadio = findViewById(R.id.pressureUnitRadioGroup);
+        convertTypeFrom = ((RadioButton) findViewById(typeRadio.getCheckedRadioButtonId())).getText().toString().toLowerCase();
+
+        switch(convertTypeFrom){
+            case "torr":
+                Toast toast=Toast. makeText(getApplicationContext(),"this shit works",Toast. LENGTH_SHORT);
+                toast. setMargin(50,50);
+                toast. show();
+        }
     }
+
+
+
+
 }
 
 
