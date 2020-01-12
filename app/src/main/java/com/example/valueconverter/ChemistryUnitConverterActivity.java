ec38cf88c866634a853ee5e111f8c432ddeb5d61
TylerLeung <leung.tyler008@gmail.com>
2020-01-11 20:29:36 -0500

    Update ChemistryUnitConverterActivity.java

--- app/src/main/java/com/example/valueconverter/ChemistryUnitConverterActivity.java
+++ app/src/main/java/com/example/valueconverter/ChemistryUnitConverterActivity.java
@@ -3,7 +3,11 @@ package com.example.valueconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.math.*;
 
@@ -13,8 +17,28 @@ public class ChemistryUnitConverterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chemistry_unit_converter);
 
-        addListenerOnSpinnerItemSelection();
-        addListenerOnButton();
+        Spinner spin = (Spinner) findViewById(R.id.spinner);
+        spin.setOnItemSelectedListener(new OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view,
+                                       int position, long id) {
+                Object item = adapterView.getItemAtPosition(position);
+                if (item != null) {
+                    Toast.makeText(ChemistryUnitConverterActivity.this, item.toString(),
+                            Toast.LENGTH_SHORT).show();
+                }
+                Toast.makeText(ChemistryUnitConverterActivity.this, "Selected",
+                        Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                // TODO Auto-generated method stub
+
+            }
+        });
     }
 
 
